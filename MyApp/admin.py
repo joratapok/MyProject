59c4012b27d6f101016c8038df1c4d1a6c7e90d3
joratapok <joratapok@gmail.com>
2020-12-29 23:46:26 +0700

    add model user

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from MyApp.models import Comments
+from MyApp.models import Comments, Profile
 
 
 @admin.register(Comments)
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ('owner', 'date', 'text', 'parent', 'id')
     readonly_fields = ('date',)
+
+@admin.register(Profile)
+class ProfileRegisterAdmin(admin.ModelAdmin):
+    list_display = ('user', 'photo')
+
--- MyApp/urls.py
+++ MyApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.CommentsView.as_view(), name='comment_list'),
     path('fish', views.FishView.as_view(), name='fish'),
     path('add_comment', views.AddCommentView.as_view(), name='add_comment'),
+    #path('profile', views.CommentsView.as_view(), name='profile'),
 ]
